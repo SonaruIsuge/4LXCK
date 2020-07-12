afec54480783db9e06a044da350852df1e924b5a
Sonaru_isuge <bread511206@gmail.com>
2020-07-12 16:18:35 +0800

    背包系統(6)
    第三章結束

--- RaceGame/Assets/Scriptable Objects/Items/Scripts/EquipmentObject.cs
+++ RaceGame/Assets/Scriptable Objects/Items/Scripts/EquipmentObject.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Equipment Object", menuName = "Inventory System/Items/Equipment")]
 public class EquipmentObject : ItemObject
 {
-    public int atkBonus;
-    public int defBonus;
     public void Awake()
     {
         type = ItemType.Equipment;
